6d0766ca31b1ca2f845a2abe37037cb43fc213a1
Ti-Ray <iannjoroge25@gmail.com>
2023-05-31 10:31:59 +0300

    First commit

---
+++ Log in page GUI.py
@@ -0,0 +1,14 @@
+import customtkinter
+from customtkinter import *
+from tkinter import *
+from PIL import *
+
+customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_appearance_mode("Dark")
+
+main = customtkinter.CTk()
+main.geometry("850x600")
+
+
+main.mainloop()
+
